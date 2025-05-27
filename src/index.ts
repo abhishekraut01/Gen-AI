3d7e3ed4a05360438e98a12c7be87f3b9be3c0af
Abhishek <Abhishekraut8697@gmail.com>
2025-05-28 02:00:19 +0530

    setting up genai and gemini

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.YOUR_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Explain me what is nodejs",
+  });
+  console.log(response);
+}
+
+main();
