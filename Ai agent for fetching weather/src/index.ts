de49e3fd4e7df11276e4714c18d9526b2888615e
Abhishek <Abhishekraut8697@gmail.com>
2025-09-13 23:49:21 +0530

    build ai agent to fetch weather details

--- Ai agent for fetching weather/src/index.ts
+++ Ai agent for fetching weather/src/index.ts
@@ -1,16 +1,98 @@
 import OpenAI, { NotFoundError } from "openai";
 import dotenv from 'dotenv'
-
+import readlineSync from "readline-sync"
 dotenv.config()
 
-const client  = new OpenAI({
-    apiKey:process.env.GEMINI_API_KEY
+const client = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY
 })
 
 
-async function fetchWeather(city:string){
+async function fetchWeather(city: string) {
     let res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}&aqi=no`)
     let data = await res.json()
     return data?.current?.temp_c;
 }
 
+let system_prompt = `
+You are the helpfull assistent and answer the question of user whatever they want and if they want current weather information
+then call the tool fetchWeather with input city for which user asking 
+
+suppose user ask for what is weather in delhi 
+so you will respond with {
+needFuntionCall : "true",
+city : "delhi"
+}
+
+after you use tool
+
+needFuntionCall : "false",
+city : "delhi"
+output : ""
+}
+
+`
+
+var UserPrompt = readlineSync.question("<<");
+
+type messageHistoryType = {
+    role: string
+    content: string
+}
+
+let messageHistory: messageHistoryType[] = [
+    {
+        role: "user",
+        content: UserPrompt
+    }
+]
+
+
+const response = await client.chat.completions.create({
+    model: "gemini-2.5-flash",
+    messages: [
+        {
+            role: "user",
+            content: UserPrompt
+        },
+        {
+            role: "system",
+            content: system_prompt
+        }
+    ]
+})
+
+messageHistory.push({
+    role: "developer",
+    content: JSON.stringify(response.choices[0]?.message.content as string)
+})
+
+let call = JSON.parse(response.choices[0]?.message.content as string)
+
+async function processAI() {
+    while (true) {
+
+        if (call.needFuntionCall) {
+            let responseInFN = await fetchWeather(call.city)
+            messageHistory.push({
+                role: "developer",
+                content: JSON.stringify(responseInFN.choices[0]?.message.content as string)
+            })
+        } else {
+            return call.output
+        }
+
+        call = client.chat.completions.create({
+            model: "gemini-2.5-flash",
+            messages: [
+
+                {
+                    role: "system",
+                    content: system_prompt
+                }
+            ]
+        })
+
+    }
+}
+processAI()
