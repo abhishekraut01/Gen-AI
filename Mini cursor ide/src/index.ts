88dc848d392b83b32ea2cf7b7826173cf246bdd2
Abhishek <Abhishekraut8697@gmail.com>
2025-09-15 17:34:43 +0530

    add basic code to call gemini

--- Mini cursor ide/src/index.ts
+++ Mini cursor ide/src/index.ts
@@ -1,4 +1,25 @@
-import OpenAI from "openai";
+import OpenAI, { NotFoundError } from "openai";
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+const client = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+})
+
+async function main() {
+    const response =await client.chat.completions.create({
+        model: "gemini-2.0-flash",
+        messages: [
+            {
+                role: "user",
+                content: "hey how are you"
+            }
+        ]
+    })
+
+    console.log(response.choices[0]?.message.content)
+}
+
+main()
